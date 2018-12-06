1c42caca13da3eb56f3e75911ec267203a9f331c
gabriel.griesser <gabriel.griesser@he-arc.ch>
2018-12-06 14:01:03 +0100

    Add tab to save board

---
+++ OthelloAlainGabriel/OthelloAlainGabriel/OthelloAlainGabriel/Token.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Drawing;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace OthelloAlainGabriel
+{
+    //Classe définissant un token
+    public class Token
+    {
+        //public string PathToToken { get; }
+        public ImageBrush ImgBrush { get; }
+        
+        public Token(string path)
+        {
+            //PathToToken = path;
+            Console.WriteLine(path);
+            Image img = Image.FromFile(path);
+            Bitmap bmp = new Bitmap(img);
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(),
+                                                                            IntPtr.Zero,
+                                                                            Int32Rect.Empty,
+                                                                            BitmapSizeOptions.FromEmptyOptions()
+            );
+            bmp.Dispose();
+            ImgBrush = new ImageBrush(bitmapSource);
+        }
+    }
+}
